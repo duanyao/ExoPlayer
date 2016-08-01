7aacdf6b40f31a6b9147c04a4b4e80b4161319be
Duan Yao <duanyao@ustc.edu>
2016-08-01 19:56:30 +0800

    Fix H.264 intra refresh live stream playback problem (issue #1654).

--- library/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
+++ library/src/main/java/com/google/android/exoplayer2/mediacodec/MediaCodecRenderer.java
@@ -456,6 +456,8 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
     }
   }
 
+  protected final Format getFormat() { return format; }
+
   @Override
   protected void onStarted() {
     // Do nothing. Overridden to remove throws clause.
@@ -885,7 +887,6 @@ public abstract class MediaCodecRenderer extends BaseRenderer {
         return false;
       }
     }
-
     if (processOutputBuffer(positionUs, elapsedRealtimeUs, codec, outputBuffers[outputIndex],
         outputIndex, outputBufferInfo.flags, outputBufferInfo.presentationTimeUs,
         shouldSkipOutputBuffer)) {
